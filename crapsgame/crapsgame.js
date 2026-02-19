764ccf58a036d3271f4b72ffcea5d506e904a79f
Victor Nwosu <uzyatforums@gmail.com>
2026-02-19 16:29:01 +0100

    implemented constants for element IDs in crapsgame.js

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -4,21 +4,17 @@ const crapsRegistrationPane = "craps-registration-pane"
 const crapsMainSection = "craps-main-section"
 
 
-
 function registerCrapsPlayer() {
     let crapsusername = document.getElementById(crapsUsernameInput).value
     removeRegistrationPane()
     showMainGameSection()
 }
+
 function removeRegistrationPane() {
     document.getElementById(crapsRegistrationPane).style.display = "none"
-    alert(crapsusername)
 }
 
 function showMainGameSection() {
     document.getElementById(crapsMainSection).style.display = "block"
-    alert(crapsusername)
-
 }
-
 
