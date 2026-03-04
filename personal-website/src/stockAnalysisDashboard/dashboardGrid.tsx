6e71187f0167c17b443cfa425cdd6e2c422a30dc
Victor Nwosu <uzyatforums@gmail.com>
2026-03-04 21:54:28 +0100

    Added Chartjs line chart for historical stock price

--- personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
+++ personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
@@ -3,6 +3,7 @@ import 'gridstack/dist/gridstack.min.css';
 import { GridStack } from 'gridstack';
 import { DashboardGridContent } from './stockAnalysisDashboard'
 import NumberStat from './numberStat';
+import LineChartContent from './lineChartContent'
 
 function DashboardGrid({ stockData }: { stockData: any }) {
   useEffect(() => {
@@ -49,9 +50,17 @@ function DashboardGrid({ stockData }: { stockData: any }) {
             ></NumberStat>
           </DashboardGridContent>
         </div>
+        {/* Second Row */}
+        <div className="grid-stack-item" gs-w="10" gs-h="3">
+          <DashboardGridContent className="grid-stack-item-content">
+            <LineChartContent
+              priceHistory={stockData.priceHistory}
+            ></LineChartContent>
+          </DashboardGridContent>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 
